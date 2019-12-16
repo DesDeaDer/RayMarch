41251107d31f8bceea7551c52ff108121dd97fb8
Kwikswik <IvanKramarenko@outlook.com>
2019-12-16 17:23:34 +0200

    basic

---
+++ Assets/Scripts/RayMarchCamera.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RayMarchCamera : MonoBehaviour
+{
+    #region Data
+#pragma warning disable 0649
+
+    [SerializeField] Shader _shader;
+    [SerializeField] Material _material;
+    [SerializeField] Camera _camera;
+
+    [SerializeField] float _rayMarchAccuracy;
+
+#pragma warning restore 0649\
+    #endregion
+
+    public Shader Shader => _shader;
+    public Material Material => _material;
+    public Camera Camera => _camera;
+
+    public float RayMarchAccuracy => _rayMarchAccuracy;
+
+    int PropertyIdCameraFrustrum { get; } = Shader.PropertyToID("_CameraFrustrum");
+    int PropertyIdCameraToWorldMartix { get; } = Shader.PropertyToID("_CameraToWorldMartix");
+    int PropertyIdCameraMaxDistance { get; } = Shader.PropertyToID("_CameraMaxDistance");
+    int PropertyIdRayMarchAccuracy { get; } = Shader.PropertyToID("_RayMarchAccuracy");
+
+    Vector3[] Quadpositions { get; } = new Vector3[]
+    {
+        new Vector3(0, 0, 3 ),
+        new Vector3(1, 0, 2 ),
+        new Vector3(1, 1, 1 ),
+        new Vector3(0, 1, 0 ),
+    };
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Material.SetMatrix(PropertyIdCameraFrustrum, Frustrum);
+        Material.SetMatrix(PropertyIdCameraToWorldMartix, Camera.cameraToWorldMatrix);
+        Material.SetFloat(PropertyIdCameraMaxDistance, Camera.farClipPlane);
+        Material.SetFloat(PropertyIdRayMarchAccuracy, RayMarchAccuracy);
+
+        RenderTexture.active = destination;
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        Material.SetPass(0);
+        GL.Begin(GL.QUADS);
+
+        //down left
+        GL.MultiTexCoord2(0, 0, 0);
+        GL.Vertex(Quadpositions[0]);
+        //down right
+        GL.MultiTexCoord2(0, 1, 0);
+        GL.Vertex(Quadpositions[1]);
+        //up right
+        GL.MultiTexCoord2(0, 1, 1);
+        GL.Vertex(Quadpositions[2]);
+        //up left
+        GL.MultiTexCoord2(0, 0, 1);
+        GL.Vertex(Quadpositions[3]);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    Matrix4x4 Frustrum
+    {
+        get
+        {
+            const float DEG2RAD_HALF = Mathf.Deg2Rad * 0.5f;
+
+            var up = Mathf.Tan(Camera.fieldOfView * DEG2RAD_HALF);
+            var right = up * Camera.aspect;
+
+            return new Matrix4x4()
+            {
+                m00 = -right,
+                m01 = up,
+                m02 = -1,
+
+                m10 = right,
+                m11 = up,
+                m12 = -1,
+
+                m20 = right,
+                m21 = -up,
+                m22 = -1,
+
+                m30 = -right,
+                m31 = -up,
+                m32 = -1,
+            };
+        }
+    }
+}
---
+++ Assets/Shaders/RayMarching.shader
@@ -0,0 +1,130 @@
+﻿Shader "RayMarching"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+	}
+
+		SubShader
+	{
+		Cull Off
+		ZWrite Off
+		ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 3.0
+
+			#include "UnityCG.cginc"
+
+			uniform float _CameraMaxDistance;
+			uniform float4x4 _CameraToWorldMartix;
+			uniform float4x4 _CameraFrustrum;
+			uniform float _RayMarchAccuracy = 0.001;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float3 ray : TEXCOORD1;
+			};
+
+			v2f vert(appdata v)
+			{
+				half index = v.vertex.z;
+				v.vertex.z = 0;
+
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				o.ray = _CameraFrustrum[(int)index].xyz;
+				o.ray /= abs(o.ray.z);
+				o.ray = mul(_CameraToWorldMartix, o.ray);
+
+				return o;
+			}
+
+			float Sphere(float3 p, float s)
+			{
+				return length(p) - s;
+			}
+
+			float GetDistance(float3 p)
+			{
+				float3 pos = p - float3(0, sin(_Time.z), 0);
+
+				pos = fmod(abs(pos), .45 + sin(_Time.z / 2) * .2) + .5;
+
+				float sphere1 = Sphere(pos, 0.05);
+				float sphere2 = Sphere(p - float3(0, -sin(_Time.z), 0), 4.25);
+				return lerp(sphere1, sphere2, .35 + sin(_Time.z * 3) / 3.14 * .25);
+			}
+
+			float3 GetNormal(float3 p)
+			{
+				float d = GetDistance(p);
+				float2 e = float2(_RayMarchAccuracy, 0);
+
+				return normalize(d - float3 (GetDistance(p - e.xyy), GetDistance(p - e.yxy), GetDistance(p - e.yyx)));
+			}
+
+			float GetLigth(float3 p)
+			{
+				float3 lp = float3(0, 2, 0);
+				
+				float3 l = normalize(lp - p);
+				float3 n = GetNormal(p);
+
+				return abs(dot(n, 1));
+			}
+
+			fixed4 RayMarching(float3 ro, float3 rd)
+			{
+				fixed4 result = fixed4(1, 1, 1, 1);
+				const int max_iteration = 128;
+
+				float t = 0;
+
+				for (int i = 0; i < max_iteration; i++)
+				{
+					if (t > _CameraMaxDistance)
+					{
+						result = fixed4(rd, 1);
+						break;
+					}
+
+					float3 p = ro + rd * t;
+					float d = GetDistance(p);
+					if (d < _RayMarchAccuracy)
+					{
+						float light = GetLigth(p);
+						result = fixed4(light, light, light, 1);
+						break;
+					}
+
+					t += d;
+				}
+
+				return result;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				float3 rayDir = normalize(i.ray);
+				float3 rayOrigin = _WorldSpaceCameraPos;
+
+				return RayMarching(rayOrigin, rayDir);
+			}
+			ENDCG
+		}
+	}
+}
